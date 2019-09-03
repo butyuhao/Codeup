9b8186006ec27ab17e80a7b83ab1470eb66a79ed
butyuhao <butyuhao@hotmail.com>
2019-09-03 18:48:49 +0800

    update

---
+++ lian_biao/cmake-build-debug/CMakeFiles/lian_biao.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lian_biao.dir/main.cpp.o"
+  "lian_biao.pdb"
+  "lian_biao"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lian_biao.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
